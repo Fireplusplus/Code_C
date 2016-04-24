d76e0c51fb0fa5019dddbbf62539e509a530f4a6
Fireplusplus <1259039012@qq.com>
2016-04-24 15:50:14 +0800

    Added files via upload

---
+++ find_first_appear_once.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <assert.h>
+
+int find_first_appear_once(const char *str)
+{
+	char hash[128] = {0};
+	char *tmp = (char *)str;
+	assert(str);
+
+	while ('\0' != *tmp)
+	{
+		hash[*tmp]++;
+		tmp++;
+	} 
+	while ('\0' != *str)
+	{
+		if (1 == hash[*str])
+		{
+			break;	
+		}
+		else
+			str++;
+	}
+
+	return *str;
+}
+
+int main()
+{
+	char arr[10] = "abcedabc";
+
+	int ret = find_first_appear_once(arr);
+	printf("%c\n", ret);
+	
+	return 0;
+}
