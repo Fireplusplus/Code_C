89b42116d44f40e5dd1c28dea4f73ca8ae1444a6
Fireplusplus <1259039012@qq.com>
2016-05-31 12:14:32 +0800

    Add files via upload

---
+++ ProgressBar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h> 
+
+void ProgressBar()
+{
+	char arr[102] = {'\0'};
+	char imag[] = "|/-\\";
+	int n = 100, i = 0;
+	while (i <= n)
+	{
+		arr[i] = '=';
+		printf("[%-100s][%d%%][%c]\r", arr, i, imag[i % 4]);
+		fflush(stdout);
+		i++;
+		usleep(100000);
+	}
+	printf("\n");
+}
+
+int main()
+{
+	ProgressBar();
+
+	return 0;
+}
